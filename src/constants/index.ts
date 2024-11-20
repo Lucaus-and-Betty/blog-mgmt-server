59d32545ac7783d9e529ec1dac2f8bd43a49abcd
betty <wusehun94zlj@gmail.com>
2024-11-20 13:08:02 +0800

    feat: ✨ 添加环境变量

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,17 +1,24 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { Users } from '../apis/users/users.entities';
-import { New } from '../apis/news/news.entities';
+import * as dotenv from 'dotenv';
+
+if (process.env.RUNNING_ENV !== 'prod') {
+  dotenv.config({ path: `.env.dev` });
+} else {
+  dotenv.config({ path: '.env.prod' });
+}
+
+console.log(process.env.NODE_ENV_DB_HOST);
 
 export const DB_CONFIG: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '123456',
+  host: process.env.NODE_ENV_DB_HOST,
+  port: Number(process.env.NODE_ENV_DB_PORT),
+  username: process.env.NODE_ENV_DB_USER,
+  password: process.env.NODE_ENV_DB_PASSWORD,
   database: 'blog_db',
   autoLoadEntities: true,
   synchronize: false,
-  entities: [Users, New],
+  entities: [],
 };
 
 export const JWT_CONFIG = {
